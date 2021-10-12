a1830614b9161c1f415a8198491a079f46734510
dotSality <90727519+dotSality@users.noreply.github.com>
2021-10-13 02:08:42 +0300

    Updates

---
+++ precache-manifest.68fdc100eba04ee10c397c39aba23f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3043575237b134ea898f59da8b53933",
+    "url": "/HW1/index.html"
+  },
+  {
+    "revision": "6318871770882e77cb75",
+    "url": "/HW1/static/css/main.2072f307.chunk.css"
+  },
+  {
+    "revision": "73bb2c98156fb4c2f0ab",
+    "url": "/HW1/static/js/2.43ed494c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW1/static/js/2.43ed494c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6318871770882e77cb75",
+    "url": "/HW1/static/js/main.2b4cc030.chunk.js"
+  },
+  {
+    "revision": "581869645ae525217d5f",
+    "url": "/HW1/static/js/runtime-main.15033f65.js"
+  }
+]);

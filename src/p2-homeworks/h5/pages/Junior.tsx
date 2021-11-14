b0070a5a379fe6d6419e543ba4fe573f7f729c7d
dotSality <90727519+dotSality@users.noreply.github.com>
2021-11-14 22:06:44 +0300

    commit with no matter

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from '../Navigation.module.css';
+
+export function Junior() {
+    return (
+        <div>
+            <div className={s.empty}></div>
+            JUNIOR PAGE
+
+        </div>
+    )
+}
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from '../Navigation.module.css';
+
+export function JuniorPlus() {
+    return (
+        <div>
+            <div className={s.empty}></div>
+            JUNIOR PLUS PAGE
+
+        </div>
+    )
+}

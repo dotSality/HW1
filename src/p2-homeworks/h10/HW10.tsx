c73676f0c56d53ab5a8214d581b55512bec31bef
dotSality <90727519+dotSality@users.noreply.github.com>
2021-12-06 17:56:49 +0300

    HW10's logic has been completed. Gonna make some cosmetic changes

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from 'react-redux';
+import store, {AppStoreType} from './bll/store';
+import {loadingAC} from './bll/loadingReducer';
 
 function HW10() {
     // useSelector, useDispatch
-    const loading = false
+    let dispatch = useDispatch()
+    let loadingState = useSelector<AppStoreType, {[key: string]: boolean}>(state => state.loading)
+    const loading = loadingState.isLoading
 
     const setLoading = () => {
         // dispatch
+        dispatch(loadingAC(true))
         // setTimeout
+        setTimeout(()=>{
+            dispatch(loadingAC(false))
+        },2000)
         console.log('loading...')
     };
 
@@ -19,9 +28,9 @@ function HW10() {
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <div style={{height: '25px'}}>крутилка...</div>
                 ) : (
-                    <div>
+                    <div style={{height: '25px'}}>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
                     </div>
                 )
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,20 @@
-const initState = {
+import React from 'react';
 
+
+const initState = {
+    isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type InitialStateType = typeof initState
+
+export const loadingReducer = (state = initState, action: IsLoadingActionType): InitialStateType => {
     switch (action.type) {
-        case '': {
-            return state
+        case 'TOGGLE-IS-LOADING': {
+            return {...state, isLoading: action.isLoading}
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (isLoading: boolean) => ({type: 'TOGGLE-IS-LOADING', isLoading} as const)
+type IsLoadingActionType = ReturnType<typeof loadingAC>
